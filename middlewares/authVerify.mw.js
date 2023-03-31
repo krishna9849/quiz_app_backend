5a87bdcb82782baec222be39c9db9a418668d4dd
krishna9849 <yellaprkrishna@gmail.com>
2023-03-31 13:49:24 +0530

    added auth verify middle ware

--- middlewares/authVerify.mw.js
+++ middlewares/authVerify.mw.js
@@ -3,6 +3,7 @@ const jwt=require('jsonwebtoken');
 const authVerify = (req,res,next)=>{
 
     const token = req.headers.authorization;
+    console.log(token);
     try{
         const decodeToken = jwt.verify(token , 'abcdef');
         req.user={id : decodeToken.id};
